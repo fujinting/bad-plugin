da688fe51a1e730c54b08d00ae301dac06ac699d
fujinting <2239216274@qq.com>
2021-07-14 13:34:44 +0800

    feat: 接收消息完成

--- extension.js
+++ extension.js
@@ -1,36 +1,36 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-const vscode = require('vscode');
+const vscode = require("vscode");
+const io = require("socket.io-client");
 
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
-
-/**
- * @param {vscode.ExtensionContext} context
- */
 function activate(context) {
+  console.log('Congratulations, your extension "bad-plugin" is now active!');
+  // 本地:
+  // let socket = io.connect("http://localhost:3000");
+  
+  // 线上:
+  let socket = io.connect("http://47.95.234.230:3001");
+  //建立连接
+  socket.on("connect", async () => {
+    console.log("连接成功");
+    //监听消息
+    socket.on("newMessage", (res) => {
+      console.log("消息来了", res);
+      vscode.window.showInformationMessage(`${res.userName}: ${res.content}`);
+    });
+  });
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "bad-plugin" is now active!');
+  let disposable = vscode.commands.registerCommand(
+    "bad-plugin.helloWorld",
+    function () {
+      vscode.window.showInformationMessage("Hello World from bad-plugin!");
+    }
+  );
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with  registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('bad-plugin.helloWorld', function () {
-		// The code you place here will be executed every time your command is executed
-
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from bad-plugin!');
-	});
-
-	context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable);
 }
 
-// this method is called when your extension is deactivated
 function deactivate() {}
 
 module.exports = {
-	activate,
-	deactivate
-}
+  activate,
+  deactivate,
+};
